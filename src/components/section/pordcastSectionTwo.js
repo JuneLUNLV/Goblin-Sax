2d33f75917058872ad6b43b26c74ff14d0a19450
June Liang <liangj10@unlv.nevada.edu>
2022-06-10 07:42:26 -0700

    podcast style v4

--- src/components/section/pordcastSectionTwo.js
+++ src/components/section/pordcastSectionTwo.js
@@ -2,10 +2,13 @@ import { Box } from '@mui/material';
 import Image from 'next/image';
 import {useState,useEffect} from 'react';
 
-
+const podcastHeader = ['Episode 1', 'Episode 2','Episode 3','Episode 4','Episode 5']
+const podcastTitle = ['Introducing the Goblin Hideout', 'Episode 2 Title','Episode 3 Title','Episode 4 Title','Episode 5 Title']
+const podcastEle = (<iframe src="https://open.spotify.com/embed/episode/2RULVxhnv54T9cfSxNo61z?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>)
 
 export const PodcastSectionTwo = (props) => {
     const [podCastPageState, setPodCastPageState] = useState(0);
+    console.log(podcastEle);
     useEffect(()=>{
 
     },[])
@@ -42,33 +45,33 @@ export const PodcastSectionTwo = (props) => {
                             <Box sx={{display:'flex',alignItems:'center',flexDirection:'column',width:"95%"}}>
                                 <Box sx={{width:'100%'}}>
                                     <h1 className="podcastBoxNumber">
-                                        Episode 1
+                                        {`${podcastHeader[podCastPageState]}`}
                                     </h1>
                                 </Box>
 
                                 <Box sx={{mt:'15px'}}>
                                     <h1 className="stepHeader">
-                                        Introducing the Goblin Hideout
+                                    {`${podcastTitle[podCastPageState]}`}
                                     </h1>
-                                </Box>
-
-                                {podCastPageState == 0 ? 
+                                </Box>   
                                     <Box sx={{width: {xs:"100%",md:'100%'},mt:"30px"}}>
-                                    <iframe src="https://open.spotify.com/embed/episode/2RULVxhnv54T9cfSxNo61z?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+                                    {podCastPageState == 0 ? <iframe src="https://open.spotify.com/embed/episode/2RULVxhnv54T9cfSxNo61z?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe> : <Box sx={{
+                                        height:'152px',
+                                        width: '100%'
+                                    }}/>}
                                     </Box>
-                                : void(0)} 
-                                <Box className="slickDots">
-                                    <Box className="slickDotActive"/>
-                                    <Box className="slickDotPresent"/>
-                                    <Box className="slickDotPresent"/>
-                                    <Box className="slickDotPresent"/>
-                                    <Box className="slickDotPresent"/>
+                                    <Box className="slickDots">
+                                        <Box className={`${podCastPageState == 0 ? "slickDotActive" : "slickDotPresent"}`} onClick={()=>setPodCastPageState(0)}/>
+                                        <Box className={`${podCastPageState == 1 ? "slickDotActive" : "slickDotPresent"}`} onClick={()=>setPodCastPageState(1)}/>
+                                        <Box className={`${podCastPageState == 2 ? "slickDotActive" : "slickDotPresent"}`} onClick={()=>setPodCastPageState(2)}/>
+                                        <Box className={`${podCastPageState == 3 ? "slickDotActive" : "slickDotPresent"}`} onClick={()=>setPodCastPageState(3)}/>
+                                        <Box className={`${podCastPageState == 4 ? "slickDotActive" : "slickDotPresent"}`} onClick={()=>setPodCastPageState(4)}/>
+                                    </Box>
                                 </Box>
                             </Box>
                         </Box>
                     </Box>
             </Box>
-        </Box>
         </section>
     )
 }
